231d29e98aaeafb71bbbfa7fa759953320f17900
mareddy45 <m.v.ramanareddy9999@gmail.com>
2020-05-30 20:02:14 -0700

    Style Marker

--- Map.py
+++ Map.py
@@ -13,7 +13,15 @@ Volcano name:<br>
 Height: %s m
 """
 
-map=folium.Map(location=[34.05,-118.246], zoom_start=3, tiles="Stamen Terrain")
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'red'
+    elif 1000<=elevation<3000:
+        return 'blue'
+    else:
+        return 'orange'
+
+map=folium.Map(location=[34.05,-118.246], zoom_start=5, tiles="Stamen Terrain")
 map.add_child(folium.Marker(location=[17.385,78.486], popup="Birth Place", icon=folium.Icon(color="green")))
 map.add_child(folium.Marker(location=[34.05,-118.246], popup="Current Place", icon=folium.Icon(color="green")))
 
@@ -21,7 +29,7 @@ fg = folium.FeatureGroup(name="Map")
 
 for lt, ln, el, name in zip(lat,lon,elev, name):
     iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
-    fg.add_child(folium.Marker(location=[lt,ln], popup=folium.Popup(iframe), icon=folium.Icon(color="red")))
+    fg.add_child(folium.CircleMarker(location=[lt,ln], radius= 6, popup=folium.Popup(iframe), fill_color=color_producer(el), color='grey', fill_opacity=0.7))
 
 map.add_child(fg)
 
