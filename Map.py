e0c3f21bf313c35ef95deee61ba25000f2d65ccd
mareddy45 <m.v.ramanareddy9999@gmail.com>
2020-05-30 18:09:48 -0700

    Added Target point on Map

--- Map.py
+++ Map.py
@@ -1,3 +1,4 @@
 import folium
-map=folium.Map(location=[38,-99], tiles="Stamen Terrain")
+map=folium.Map(location=[17.385,78.486], zoom_start=6, tiles="Stamen Terrain")
+map.add_child(folium.Marker(location=[17.385,78.486], popup="Birth Place", icon=folium.Icon(color="red")))
 map.save("Map.html")
