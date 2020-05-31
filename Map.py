ec097dea3642235ff07d29623a6525ad0adc3d22
mareddy45 <m.v.ramanareddy9999@gmail.com>
2020-05-30 18:59:05 -0700

    Added elevation

--- Map.py
+++ Map.py
@@ -4,14 +4,16 @@ import pandas
 data= pandas.read_csv("volcanoes.txt")
 lat= list(data["LAT"])
 lon= list(data["LON"])
+elev= list(data["ELEV"])
 
-map=folium.Map(location=[17.385,78.486], zoom_start=6, tiles="Stamen Terrain")
-map.add_child(folium.Marker(location=[17.385,78.486], popup="Birth Place", icon=folium.Icon(color="red")))
+map=folium.Map(location=[34.05,-118.246], zoom_start=3, tiles="Stamen Terrain")
+map.add_child(folium.Marker(location=[17.385,78.486], popup="Birth Place", icon=folium.Icon(color="green")))
+map.add_child(folium.Marker(location=[34.05,-118.246], popup="Current Place", icon=folium.Icon(color="green")))
 
 fg = folium.FeatureGroup(name="Map")
 
-for lt, ln in zip(lat,lon):
-    fg.add_child(folium.Marker(location=[lt,ln], popup="volcanoes", icon=folium.Icon(color="red")))
+for lt, ln, el in zip(lat,lon,elev):
+    fg.add_child(folium.Marker(location=[lt,ln], popup= str(el)+ " m", icon=folium.Icon(color="red")))
 
 map.add_child(fg)
 
